6d916797bd371e4955d184f27cc5197b8bdeb925
Seven-Zhan <77223486+Seven-Zhan@users.noreply.github.com>
2021-02-18 16:53:25 +0800

    add scripts
    pytorch_to_caffe.py ---> transfer pytorch to caffe
    run_convert.py        ---> convert script
    run_compare.py      ---> compare pytorch and caffe model

---
+++ run_compare.py
@@ -0,0 +1,15 @@
+import os
+import argparse
+import subprocess
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser('Convert pytorch model to caffe format')
+    parser.add_argument('-t', '--task', default='fic', type=str, help='type of task: fic or feh')
+    
+    args, unparsed = parser.parse_known_args()
+
+    script_path = os.path.join('result', args.task, 'compare.py')
+    subprocess.run(['python', script_path])
